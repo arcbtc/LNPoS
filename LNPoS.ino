e6a3a297662547b0bd9a38865ea9584fc5c1c847
Ben Arc <ben@arc.wales>
2021-04-23 15:27:19 +0100

    Added libraries I used

--- LNPoS.ino
+++ LNPoS.ino
@@ -44,6 +44,7 @@ float conversion;
 
 void setup() 
 {
+  Serial.println(115200);
   M5.begin();
   Wire.begin();
   logo_screen();
@@ -394,9 +395,10 @@ void portal()
   wm.addParameter(&custom_invoice_key);
   wm.addParameter(&custom_currency);
   wm.addParameter(&custom_wifi_password);
-
+   
 //IF RESET WAS TRIGGERED, RUN PORTAL AND WRITE FILES
   if (!wm.autoConnect("⚡LNPoS⚡", wifi_password)) {
+    
     Serial.println("failed to connect and hit timeout");
     delay(3000);
     ESP.restart();
